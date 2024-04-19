9f32779e54c37dcdf441a80e476b59ff46e346ef
Dinesh Gurjar <130591848+Dineshgurjjar@users.noreply.github.com>
2024-04-19 19:07:03 +0530

    Add files via upload

---
+++ java/dinesh.java
@@ -0,0 +1,14 @@
+/**
+ * dinesh
+ */
+import java.util.*;
+public class dinesh {
+
+    public static void main (String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("hello Dinesh Gurjar Enter the value of the  value  ");
+        int n  = obj.nextInt();
+        System.out.println("The output is "+n);
+        
+    }
+}
